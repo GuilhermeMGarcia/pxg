2b70d1692c1e596f4924a77c98a8c4fd53a331c8
GuilhermeGarcia <guilhermegarci4@gmail.com>
2021-10-14 14:26:19 -0300

    salva e carregar

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pickle
+
 from pessoas import *
 
 
@@ -27,10 +29,81 @@ def escolher_pokemon(player):
             print('Escolha um pokemon!')
 
 
-player = Player()
-player.mostra_dinheiro()
-escolher_pokemon(player)
-player.mostrar_pokemon()
-inimigo = Inimigo(nome=None, pokemons=[PokemonEletrico('Raichu', lvl=1)])
-player.batalhar(inimigo)
+def salvar_jogo(player):
+    arquivo = "database.db"
+    try:
+        with open(arquivo, "wb") as obj_arquivo:
+            pickle.dump(arquivo, obj_arquivo)
+            print('Jogo salvo com sucesso')
+    except Exception as error:
+        print('Erro ao Salvar o jogo')
+        print(error)
+
+
+def carregar_jogo(player):
+    arquivo = "database.db"
+    try:
+        with open(arquivo, "rb") as obj_arquivo:
+            player = pickle.load(obj_arquivo)
+            return player
+            print('Jogo salvo com sucesso')
+    except Exception as error:
+        print('Erro ao Carregar o jogo')
+        print(error)
+
+
+if __name__ == "__main__":
+    print('-------------------------------------')
+    print('Seja bem vindo ao PXG')
+    print('-------------------------------------')
+
+    nome = input('Digite seu nome:')
+    player = Player(nome)
+    print(f'{nome} parabens, esse é o mundo dos pokemons, voce tera que explorar'
+          f' e se tornar o mestre dos pokemons')
+    player.mostra_dinheiro()
+
+    print('Capture o maximo de pokemons que conseguir')
+    if player.pokemons:
+        print('Vi que voce ja possui pokemon')
+        player.mostrar_pokemon()
+    else:
+        print('Voce n possui pokemon agora voce podera escolher um')
+        escolher_pokemon(player)
+
+    print('Pronto, agora que vc possui um pokemon tera que lutar contra Gary seu amigo de infancia')
+    gary = Inimigo('Gary', [PokemonAgua('squirtle', 1)])
+    player.batalhar(gary)
+
+    while True:
+        print('--------------------------')
+        print('Oque deseja fazer')
+        print('1- Explorar o mundo')
+        print('2- Batalhar com inimigo')
+        print('3- Mostrar dinheiro')
+        print('4- Mostrar Pokemons')
+        print('5- Salvar Jogo')
+        print('6- Carregar Jogo')
+        print('0- Sair do jogo')
+        escolha = input('Escolha:')
+
+        if escolha == '0':
+            print('Fecando o jogo...')
+            break
+        elif escolha == '1':
+            player.exlorar()
+        elif escolha == '2':
+            inimigo_aleatorio = Inimigo()
+            player.batalhar(inimigo_aleatorio)
+        elif escolha == '3':
+            player.mostra_dinheiro()
+        elif escolha == '4':
+            player.mostrar_pokemon()
+        elif escolha == '5':
+            salvar_jogo(player)
+        elif escolha == '6':
+            carregar_jogo(player)
+        else:
+            print('Escolha invalida')
+
 
